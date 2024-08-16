16b25876fed5c13b3aa47f72d035d0ed8bc8d20a
Lucas Linhares <lucas_linhares_@hotmail.com>
2024-08-16 17:18:42 -0300

    ajustes

--- index.ts
+++
@@ -1,4 +0,0 @@
-import logger from './src/utils/logger';
-import './jobs/syncData';
-
-logger.info('API RODANDO.');
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,8 +3,11 @@ import winston from 'winston';
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
+    winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level}: ${message}`;
+    })
   ),
   transports: [
     new winston.transports.Console(),
