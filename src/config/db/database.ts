d60c1a19362cea47836a12fdabbd48a6a262ec9f
Lucas Linhares <lucas_linhares_@hotmail.com>
2024-08-20 17:32:30 -0300

    database e dotenv

---
+++ src/config/db/database.ts
@@ -0,0 +1,25 @@
+import firebird from 'node-firebird';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const options: firebird.Options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT!) ,
+  database: process.env.DB_DATABAS,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  pageSize: parseInt(process.env.DB_PAGE_SIZE!)
+};
+
+export const getDatabaseConnection = (): Promise<firebird.Database> => {
+  return new Promise<firebird.Database>((resolve, reject) => {
+    firebird.attach(options, (err, db) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(db);
+      }
+    });
+  });
+};
