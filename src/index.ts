ea137c7fd8cecabb862b490965d06a4ab14ec209
Lucas Linhares <lucas_linhares_@hotmail.com>
2025-01-21 16:50:48 -0300

    detalhe porta

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { atualizarDadosEnderecoController } from './controllers/integracao/atual
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 const NAME_BASE_URL = 'api-infobrasil-tray'
 
 app.use(express.json());
