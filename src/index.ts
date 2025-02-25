ab4084462ae5b14501756b0838ea523a824f3983
Lucas Linhares <lucas_linhares_@hotmail.com>
2025-02-25 16:53:53 -0300

    reduzindo 3 horas do utc gerado pelo dayjs

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import logger from './utils/logger';
 import { inicializarLoja } from './controllers/integracao/inicializarLoja';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
 import { atualizarLoja } from './controllers/integracao/atualizarLoja';
 import { agendadorJobs, limparFila } from './queue/queue';
 import { atualizarDadosEnderecoController } from './controllers/integracao/atualizarDadosEnderecoController';
-import { reiniciarUltimasSincronizacoes } from './utils/horariosSincronizacoes';
 
 dotenv.config();
 
@@ -16,8 +13,6 @@ const PORT = process.env.PORT || 3333;
 const NAME_BASE_URL = 'api-infobrasil-tray'
 
 app.use(express.json());
-
-dayjs.extend(utc);
 
 app.get(`/`, (req, res) => {
   res.send('API RODANDO.');
--- src/utils/horariosSincronizacoes.ts
+++ src/utils/horariosSincronizacoes.ts
@@ -2,18 +2,23 @@ import dayjs from "dayjs";
 import logger from "./logger";
 
 let ultimaSincronizacaoEstoques: string | null = null;
+
 export function atualizarUltimaSincronizacaoEstoques() {
-    ultimaSincronizacaoEstoques = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  ultimaSincronizacaoEstoques = dayjs().subtract(3, "hour").format("YYYY-MM-DD HH:mm:ss");
 }
+
 export function obterUltimaSincronizacaoEstoques() {
-    return ultimaSincronizacaoEstoques;
+  return ultimaSincronizacaoEstoques;
 }
 
 export function reiniciarUltimasSincronizacoes() {
-    ultimaSincronizacaoEstoques = dayjs().subtract(3, 'minute').format("YYYY-MM-DD HH:mm:ss");
-    logger.log({
-        level: 'info',
-        message: `Ultima sincronização reiniciada: ${ultimaSincronizacaoEstoques}`,
-    });
+  ultimaSincronizacaoEstoques = dayjs()
+    .subtract(3, "minute")
+    .subtract(3, "hour")
+    .format("YYYY-MM-DD HH:mm:ss");
 
+  logger.log({
+    level: "info",
+    message: `Ultima sincronização reiniciada: ${ultimaSincronizacaoEstoques}`,
+  });
 }
