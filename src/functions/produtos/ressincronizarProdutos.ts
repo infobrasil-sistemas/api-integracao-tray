b71f6c18799860dc98ee2ed7b2f2603b708f4448
Lucas Linhares <lucas_linhares_@hotmail.com>
2025-09-23 09:26:34 -0300

    detalhe

--- src/functions/produtos/ressincronizarProdutos.ts
+++ src/functions/produtos/ressincronizarProdutos.ts
@@ -10,6 +10,7 @@ import logger from "../../utils/logger";
 export async function ressincronizarProdutos(loja: ILojaTray, conexao: IConnectionOptions, accessToken: string) {
     try {
         const produtos = await getAllProducts(loja, accessToken)
+        console.log(produtos)
         let totalProdutosRessincronizados = 0
         // for (const produto of produtos) {
         //     const pro_codigo: string | null = await ressincronizarProduto(loja, conexao, produto)
