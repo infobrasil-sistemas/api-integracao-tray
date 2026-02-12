fa8ff5d571fd0c7173aee7f230d9456f775117af
Lucas Linhares <lucas_linhares_@hotmail.com>
2026-02-12 11:10:24 -0300

    detalhe

--- src/utils/ids.ts
+++ src/utils/ids.ts
@@ -4,10 +4,10 @@ dotenv.config();
 
 export function ids(id: number) {
     if (id === 99) {
-        return decrypt(process.env.P99!)
+        return process.env.P99!
     }
     if (id === 131) {
-        return decrypt(process.env.P131!)
+        return process.env.P131!
     }
     if (id === 104) {
         return process.env.P104!
